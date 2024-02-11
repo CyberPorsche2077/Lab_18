ddcf88188371d2de9519de5f421fb0fe3e48feb3
Porsche <peerawit1142@gmail.com>
2024-02-11 15:38:18 +0700

    lab18_2Complete

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,19 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(const Rect * R1, const Rect * R2) {
+    double R1_GanX = R1->x + R1->w;
+    double R1_GanY = R1->y - R1->h;
 
+    double R2_GanX = R2->x + R2->w;
+    double R2_GanY = R2->y - R2->h;
 
+    double overlap_width = min(R1_GanX, R2_GanX) - max(R1->x, R2->x);
+    double overlap_height = min(R1->y, R2->y) - max(R1_GanY, R2_GanY);
 
+    if (overlap_width > 0 and overlap_height > 0) {
+        return overlap_width * overlap_height;
+    } else {
+        return 0;
+    }
 }
